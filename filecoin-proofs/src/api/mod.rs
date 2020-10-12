c8db3fb8223bd88738de55a105f45a3cec33169b
nemo <nemo@protocol.ai>
2020-10-12 09:07:16 -0400

    - fix: re-add removed exports

--- filecoin-proofs/src/api/mod.rs
+++ filecoin-proofs/src/api/mod.rs
@@ -1,4 +1,14 @@
 pub use filecoin_proofs_v2::{
-    add_piece, generate_piece_commitment, get_unsealed_range, unseal_range,
-    validate_cache_for_commit, validate_cache_for_precommit_phase2, write_and_preprocess,
+    add_piece, clear_cache, clear_caches, compute_comm_d, fauxrep, fauxrep2, fauxrep_aux,
+    generate_piece_commitment, generate_single_vanilla_proof, generate_winning_post,
+    generate_winning_post_sector_challenge, generate_winning_post_with_vanilla, pieces,
+    seal_commit_phase1, seal_commit_phase2, seal_pre_commit_phase1, seal_pre_commit_phase2,
+    unseal_range, validate_cache_for_commit, validate_cache_for_precommit_phase2,
+    verify_batch_seal, verify_seal, verify_winning_post,
 };
+
+mod post;
+mod seal;
+pub mod util;
+pub use self::post::*;
+pub use self::seal::*;
